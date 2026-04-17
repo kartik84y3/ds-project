bc84b3c59ae37da3bf44ca75167cb81796254923
kartik <karti@kartis-MacBook-Air.local>
2026-04-18 04:36:02 +0530

    force redeploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+console.log("CORS enabled version");
+
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
